34454f8dc02291ac989f15850e6e9f1fe7124b60
allenn <Neil.Allen@insphire.onmicrosoft.com>
2024-10-17 17:15:23 +0100

    initial

---
+++ pinewood_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pinewood_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
